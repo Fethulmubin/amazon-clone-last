c437191892e4fcd46a88aaa1c5d8de525a8545d8
Fethulmubin <fetihul@gmail.com>
2024-06-27 14:32:41 +0300

    authorization added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
 import Routering from "./Routering";
+import react, {useEffect, useContext} from 'react'
 import {auth} from './Utility/Firebase'
-import {Type} from './Utility/Firebase'
+import {Type} from './Utility/action.type'
+import { DataContext } from "./Component/DataProvider/DataProvider";
+import { useContext, useEffect } from "react";
 
-
+const [{user}, dispatch] = useContext(DataContext);
 function App() {
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser)=>{
+    if(authUser){
+      dispatch({
+        type: Type.SET_USER,
+        user: authUser
+      })
+    }
+    else{
+      dispatch({
+        type: Type.SET_USER,
+        user: null
+      })
+    }
+    })
+  },[])
   return (
     <>
       <Routering />
